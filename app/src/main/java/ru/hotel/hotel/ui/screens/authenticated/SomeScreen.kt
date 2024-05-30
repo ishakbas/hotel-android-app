0cefca832c6008c7a8152bb7d6daa186843a5b08
Michael <a@>
2024-05-30 22:23:23 +0300

    removed useless file

--- app/src/main/java/ru/hotel/hotel/ui/screens/authenticated/SomeScreen.kt
+++
@@ -1,9 +0,0 @@
-package ru.hotel.hotel.ui.screens.authenticated
-
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-
-@Composable
-fun SomeScreen(modifier: Modifier = Modifier) {
-
-}
